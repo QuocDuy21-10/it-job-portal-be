575fbff54bb52bb27ff1322a36e0e891f0d1539a
duydq2110 <quocduy37na@gmail.com>
2025-10-08 20:35:31 +0700

    feat(user): update user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
+import mongoose from 'mongoose';
 
-export class CreateUserDto {
+class Company {
+  @IsNotEmpty({ message: 'Company ID is required' })
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty({ message: 'Company name is required' })
+  name: string;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
   @IsEmail({}, { message: 'Email is invalid' })
   @IsNotEmpty({ message: 'Email is required' })
   email: string;
@@ -8,7 +21,42 @@ export class CreateUserDto {
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
 
+  @IsNotEmpty({ message: 'Age is required' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender is required' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Address is required' })
+  address: string;
+}
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
+  @IsEmail({}, { message: 'Email is invalid' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Age is required' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender is required' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Address is required' })
   address: string;
+
+  @IsNotEmptyObject({ nullable: true }, { message: 'Company is required' })
+  @IsObject({ message: 'Company must be an object' })
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+
+  @IsNotEmpty({ message: 'Role is required' })
+  role: string;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,10 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
+  @Prop({ required: true })
+  name: string;
+
   @Prop({ required: true, unique: true })
   email: string;
 
@@ -12,16 +15,43 @@ export class User {
   password: string;
 
   @Prop()
-  name: string;
+  age: number;
 
   @Prop()
-  phone: string;
-
-  @Prop()
-  age: string;
+  gender: string;
 
   @Prop()
   address: string;
+
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
 
   @Prop()
   createdAt?: Date;
@@ -30,10 +60,10 @@ export class User {
   updatedAt?: Date;
 
   @Prop()
-  deletedAt?: Date;
+  isDeleted: boolean;
 
   @Prop()
-  isDeleted?: boolean;
+  deletedAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
