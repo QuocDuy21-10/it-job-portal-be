f765aa8e35ffa5c15b8a709d286ea9ab43393466
duydq2110 <quocduy37na@gmail.com>
2025-10-22 10:38:14 +0700

    feat(subcriber): implement CRUD APIs

---
+++ src/subscribers/dto/create-subscriber.dto.ts
@@ -0,0 +1,15 @@
+import { IsArray, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateSubscriberDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsEmail({}, { message: 'Email is invalid' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Skills is required' })
+  @IsArray({ message: 'Skills must be an array' })
+  @IsString({ each: true, message: 'Each skill must be a string' })
+  skills: string[];
+}
---
+++ src/subscribers/subscribers.service.ts
@@ -0,0 +1,119 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateSubscriberDto } from './dto/create-subscriber.dto';
+import { UpdateSubscriberDto } from './dto/update-subscriber.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Subscriber, SubscriberDocument } from './schemas/subscriber.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import aqp from 'api-query-params';
+import mongoose from 'mongoose';
+
+@Injectable()
+export class SubscribersService {
+  constructor(
+    @InjectModel(Subscriber.name)
+    private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+  ) {}
+
+  async create(createSubscriberDto: CreateSubscriberDto, user: IUser) {
+    const { name, email, skills } = createSubscriberDto;
+    const isExist = await this.subscriberModel.findOne({ email });
+    if (isExist) {
+      throw new BadRequestException(
+        `Email: ${email} đã tồn tại trên hệ thống. Vui lòng sử dụng email khác.`,
+      );
+    }
+
+    let newSubs = await this.subscriberModel.create({
+      name,
+      email,
+      skills,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+
+    return {
+      _id: newSubs?._id,
+      createdBy: newSubs?.createdAt,
+    };
+  }
+
+  async findAll(currentPage?: number, limit?: number, query?: string) {
+    const { filter, sort, population, projection } = aqp(query);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.subscriberModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.subscriberModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .select(projection as any)
+      .populate(population)
+      .exec();
+
+    return {
+      result,
+      meta: {
+        current: currentPage,
+        pageSize: limit,
+        pages: totalPages,
+        total: totalItems,
+      },
+    };
+  }
+
+  async findOne(id: string) {
+    this.validateObjectId(id);
+
+    return await this.subscriberModel.findOne({
+      _id: id,
+    });
+  }
+
+  async update(id: string, updateSubscriberDto: UpdateSubscriberDto, user: IUser) {
+    this.validateObjectId(id);
+    const updated = await this.subscriberModel.updateOne(
+      { _id: id },
+      {
+        ...updateSubscriberDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return updated;
+  }
+
+  async remove(id: string, user: IUser) {
+    this.validateObjectId(id);
+
+    await this.subscriberModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.subscriberModel.softDelete({
+      _id: id,
+    });
+  }
+
+  private validateObjectId(id: string): void {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException(`Not found Subscriber with id = ${id}`);
+    }
+  }
+}
