b30a04fb626d15a05634fc6f3f531bf750312d38
QuocDuy21-10 <quocduy37na@gmail.com>
2025-10-25 22:27:41 +0700

    feat(health): integrate Terminus module for database health checks

---
+++ src/health/health.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { HealthCheck, HealthCheckService, MongooseHealthIndicator } from '@nestjs/terminus';
+import { Public } from 'src/decorator/customize';
+
+@ApiTags('Health Check')
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: MongooseHealthIndicator,
+  ) {}
+
+  @Get()
+  @Public()
+  @HealthCheck()
+  check() {
+    return this.health.check([() => this.db.pingCheck('database')]);
+  }
+}
